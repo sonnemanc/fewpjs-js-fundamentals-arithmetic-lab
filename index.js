e2c81caa2cf74450e7bda6ae1042bb6f96dbbbf9
Corban <sonnemanc@gmail.com>
2022-02-09 13:30:40 -0800

    commit tree 29c13f21a15420213cbb5cd6d102670076a24416

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const num3 = 10
+const num4 = 34
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num4 % num3
+
+let max = Math.max(10, 20);
